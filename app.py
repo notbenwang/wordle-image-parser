3bc6805023c7c951b05a5169fc86ba1964411ca8
notbenwang <112118953+notbenwang@users.noreply.github.com>
2024-08-20 14:38:34 -0400

    added modals

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from PIL import Image, ImageDraw
 import io
 import base64 
 import os
+# from flask_modals import Modal
 
 app = Flask(__name__)
-variable = 21
+# modal = Modal(app)
+
 def array_to_string(arr):
     string = ""
     for i, item in enumerate(arr):
@@ -42,20 +44,24 @@ def index():
         img_pil = Image.open(buffer)
         img_src = f'data:image/png;base64,{base64_image}'
         
+        
         try:
             uploaded_wordle_statistic = reader.get_wordle_statistics_from_img(file.filename, img_pil)
             uploaded_results = reader.get_data(uploaded_wordle_statistic.text, uploaded_wordle_statistic.color_grid)
+        
+        # Flash error message
         except:
             flash(f"An image was uploaded, but there was an error parsing image: {file.filename}")
             return render_template("app.html", default_url=default_url, img_src="static/wordle_images/image_497.jpg",
                             results=results, wordle_statistic=wordle_statistic)
-        
         if not uploaded_wordle_statistic or not uploaded_results:
             flash(f"An image was uploaded, but there was an error parsing image: {file.filename}")
             return render_template("app.html", default_url=default_url, img_src="static/wordle_images/image_497.jpg",
                             results=results, wordle_statistic=wordle_statistic)
 
+        # Return input file
         return render_template("app.html", default_url=file.filename, img_src=img_src, results=uploaded_results, wordle_statistic=uploaded_wordle_statistic)
     
+    # Return default
     return render_template("app.html", default_url=default_url, img_src="static/wordle_images/image_497.jpg",
                             results=results, wordle_statistic=wordle_statistic)
